0394c4490af6e9fa3d472f6894f25a0d58907f04
shinyuna <yunadev01@gmail.com>
2021-01-03 23:50:28 +0900

    #.0.3 create apollo client

---
+++ src/apollo.js
@@ -0,0 +1,22 @@
+import ApolloClient from "apollo-boost"
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      likeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: !isLiked,
+          },
+        })
+      },
+    },
+  },
+})
+
+export default client
--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+import { ApolloProvider } from "@apollo/react-hooks"
+import client from "./apollo"
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+)
